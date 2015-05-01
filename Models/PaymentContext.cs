a9a4701bd85000cadeb1d26d9a39c30b249d5b90
Thierry Fleury <thierry@codinlab.fr>
2015-05-01 19:57:07 +0200

    Add PaymentContext

---
+++ Models/PaymentContext.cs
@@ -0,0 +1,9 @@
+﻿using OShop.PayPal.Models.Api;
+
+namespace OShop.PayPal.Models {
+    public class PaymentContext {
+        public bool UseSandbox;
+        public AccessToken Token;
+        public Payment Payment;
+    }
+}
