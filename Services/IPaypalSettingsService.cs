52f6524c5b5f7207246846bebff2076d279723c1
Thierry Fleury <thierry@codinlab.fr>
2016-03-13 17:24:45 +0100

    Clean usings

--- Services/IPaypalSettingsService.cs
+++ Services/IPaypalSettingsService.cs
@@ -1,10 +1,5 @@
 ﻿using Orchard;
 using OShop.PayPal.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OShop.PayPal.Services {
     public interface IPaypalSettingsService : IDependency {
